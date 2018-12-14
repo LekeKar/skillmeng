4db206289f48c142b7df03cfd920955ffd9fcd2c
Leke karunwi <lekekarunwi@gmail.com>
2018-12-14 12:00:20 +0000

    removed drop_course payment

--- db/migrate/20180906091904_drop_table_course_payments_and_course_days.rb
+++ db/migrate/20180906091904_drop_table_course_payments_and_course_days.rb
@@ -1,6 +1,5 @@
 class DropTableCoursePaymentsAndCourseDays < ActiveRecord::Migration
   def change
     drop_table :course_days, force: :cascade
-    drop_table :course_payments, force: :cascade
   end
 end
